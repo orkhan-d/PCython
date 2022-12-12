7fd0d93bc50c2002079693c17e9532b00242a357
Orkhan Djafarli <orhan.jafarliev@gmail.com>
2022-12-12 19:18:50 +0300

    Added muting and unmuting

--- telegram_client/handlers/sound.py
+++ telegram_client/handlers/sound.py
@@ -3,6 +3,25 @@ from bot import client
 from loader import dp, types
 from configparser import ConfigParser
 
+@dp.message_handler(commands=['mute', 'unmute'])
+async def toggle_mute(message: types.Message):
+    creds = ConfigParser()
+    creds.read('config.ini')
+
+    client.connect(**(creds['CREDETIANALS']))
+    comms = [
+        'amixer -c 1 -q set Master toggle',
+        'amixer -c 1 -q set Headphone unmute',
+        'amixer -c 1 -q set Speaker unmute'
+    ]
+
+    stdin, stdout, stderr = client.exec_command(comms[0] if message.get_command()=='mute' else ' && '.join(comms))
+
+    try:
+        server_reply = stdout.read().decode()
+        await message.answer(server_reply)
+    except:
+        pass
 
 @dp.message_handler(commands=['voldown'])
 async def main(message: types.Message):
