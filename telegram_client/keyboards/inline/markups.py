2ea7105714cd286fb7ab9cac751be08c76db49d1
Orkhan Djafarli <orhan.jafarliev@gmail.com>
2022-12-13 18:01:02 +0300

    Added button controls

--- telegram_client/keyboards/inline/markups.py
+++ telegram_client/keyboards/inline/markups.py
@@ -7,6 +7,10 @@ def get_main_menu(vol_mode: bool = True):
                 InlineKeyboardButton('🔊', callback_data='volup'+('' if not vol_mode else 'm')),
                 InlineKeyboardButton('🔉', callback_data='voldown'),
                 InlineKeyboardButton(('🔇' if vol_mode else '🔈'), callback_data=('mute' if vol_mode else 'unmute')),
+            ],
+            [
+                InlineKeyboardButton('⏯', callback_data='play-pause'),
+                InlineKeyboardButton('⏹', callback_data='stop'),
             ]
         ]
     )
